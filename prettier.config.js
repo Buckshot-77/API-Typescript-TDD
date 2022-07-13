efde6fbaec96f76186c42687dabaccf1d045ff11
Hugo Oliveira <hugooliveira@profitfy.me>
2022-07-13 17:52:23 -0300

    fix: make eslint not crash when commiting

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    singleQuote: true,
-    trailingComma: 'all',
-    arrowParens: 'avoid',
-    printWidth: 100,
-}
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  printWidth: 100,
+};
