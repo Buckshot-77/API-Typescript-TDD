d0a9b3a6278a00f417ba4328b0869c7b4c431aa4
Hugo Oliveira <hugooliveira@profitfy.me>
2022-07-21 17:50:25 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -29,9 +29,24 @@ describe('Bcrypt Adapter', () => {
 
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
   });
+
   test('Should return a hash on success', async () => {
     const { sut } = makeSut();
     const hash = await sut.encrypt('any_value');
     expect(hash).toBe('hash');
+  });
+
+  test('Should throw if Bcrypt throws', async () => {
+    const { sut } = makeSut();
+
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+      return new Promise(() => {
+        throw new Error();
+      });
+    });
+
+    const promise = sut.encrypt('any_value');
+
+    await expect(promise).rejects.toThrow();
   });
 });
