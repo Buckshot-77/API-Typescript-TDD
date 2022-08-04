af11836ec43815e664d44332cdb626f52cf787e7
Hugo Oliveira <hugooliveira@profitfy.me>
2022-08-04 12:09:26 -0300

    refactor: create mongo generic collection mapper

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,7 +1,6 @@
 import { IAddAccountRepository } from '../../../../data/protocols/IAddAccountRepository';
 import { IAccountModel } from '../../../../domain/models/account';
 import { IAddAccountModel } from '../../../../domain/useCases/protocols/IAddAccount';
-import { mapAccount } from './account-mapper';
 import { MongoHelper } from '../helpers/mongo-helper';
 
 export class AccountMongoRepository implements IAddAccountRepository {
@@ -12,6 +11,6 @@ export class AccountMongoRepository implements IAddAccountRepository {
 
     const account = await accountCollection.findOne({ _id: id });
 
-    return mapAccount(account);
+    return MongoHelper.map(account);
   }
 }
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -14,4 +14,19 @@ export const MongoHelper = {
   getCollection(name: string): Collection {
     return this.client.db().collection(name);
   },
+
+  map(collection: any): any {
+    const { _id, ...collectionWithoutId } = collection;
+
+    const mappedCollection = {
+      id: _id.toString(),
+      ...collectionWithoutId,
+    };
+
+    if (mappedCollection.password) {
+      delete mappedCollection.password;
+    }
+
+    return mappedCollection;
+  },
 };
